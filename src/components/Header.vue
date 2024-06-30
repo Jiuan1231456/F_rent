6caa100d8688e6db386b78eb586df3d66cefb188
jhen0406 <lighters0406@gmail.com>
2024-06-30 12:23:07 +0800

    導覽列設定

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,11 +10,31 @@ export default{
 
 <template>
     <div class="headerArea">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink class="routerItem" to="/bill">帳單管理</RouterLink>
+        <RouterLink class="routerItem" to="/benefit">觀看收益</RouterLink>
     </div>
 </template>
 
 <style scoped lang="scss">
-
+.headerArea{
+    height: 100dvh;
+    width: 15%;
+    background-color: #FFC89A;
+    padding-top: 5%;
+.routerItem{
+    color: black;
+    background-color: #FFC89A;
+    text-decoration: none;
+    font-size: 1.2em;
+    display: flex;
+    letter-spacing: 8px;
+    justify-content: center;
+    padding: 5%;
+    &:hover{
+        cursor: pointer;
+        background-color: #FF9D60;
+        font-weight: 400;
+    }
+}
+}
 </style>
