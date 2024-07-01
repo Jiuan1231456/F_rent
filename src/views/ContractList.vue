1a13c997f7ada4971ecc6049704054a15264a8bd
Jiuan1231456 <coss2481@gmail.com>
2024-07-02 00:21:03 +0800

    更改搜尋欄樣式，新增狀態選單，和新增契約跳轉功能

--- src/views/ContractList.vue
+++ src/views/ContractList.vue
@@ -11,7 +11,11 @@ export default {
         ...mapState(dataStore, ['page'])
     },
     methods: {
-        ...mapActions(dataStore,['setPage'])
+        ...mapActions(dataStore,['returnPage','setPage']),
+        goToContractAdd() {
+            // 使用 Vue Router 的方式進行跳轉
+            this.$router.push({ name: 'contractAdd'}); // 假設路由設定中有名為 Back1 的路由
+        },
     },
     mounted(){
         this.setPage(3)
@@ -20,148 +24,188 @@ export default {
 </script>
 
 <template>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-
-<div class="manageArea">
-    <button>我在這裡</button>
-    <h2>這是租約管理列表頁面</h2>
-    <p>{{ this.page }}</p>
+    <div class="manageArea">
+    <!-- 搜尋欄 -->
     <div class="searchPlace">
-            <label>承租地址：<input class="srColumn" type="text" value="" v-model="quizFilters.name"></label>
-            <label>承租人姓名：<input class="srColumn" type="text" value="" v-model="quizFilters.name"></label>
-            <label for="start_time">租約時間 從：</label>
-            <input type="date" id="start" name="trip-start" value="" v-model="quizFilters.startDate" min="1970-01-01"
-                max="2050-12-31" />
-            <label for="end_time">到：</label>
-            <input type="date" id="start" name="trip-start" v-model="quizFilters.endDate" value=""
-                min="1970-01-01" max="2050-12-31" />
-            <button class="searchbtn" type="button" @click="search()">搜尋</button>
+        <div class="inputPlace">
+        <span class="label">承租地址　：</span>
+        <div class="InputContainer">
+            <input placeholder="Search.." id="input" class="input" name="text" type="text">
+        </div>
+        </div>
+        <div class="inputPlace">
+        <span class="label">承租人姓名：</span>
+        <div class="InputContainer">
+            <input placeholder="Search.." id="input" class="input" name="text" type="text">
+        </div>
+        </div>
+        <!-- 選擇日期 -->
+        <p class="datetime">
+        <label for="start_time">租約時間 從：</label>
+        <input type="date" id="start" name="trip-start" min="1970-01-01" max="2050-12-31" style="font-size: 22px;">
+        <label for="end_time">到：</label>
+        <input type="date" id="end" name="trip-end" min="1970-01-01" max="2050-12-31" style="font-size: 22px;">
+        <button class="searchbtn" type="button">搜尋</button>
+        </p>
     </div>
-    <!-- 新增/刪除租約 -->
-    <i class="fa-solid fa-trash-can"@click="" ></i>
-    <i class="fa-solid fa-circle-plus" @click="goToContractAdd"> </i>
-    <!-- con=contract -->
+
+    <div class="container">
+        <!-- 新增/刪除租約 -->
+        <i class="fa-solid fa-trash-can"></i>
+        <i class="fa-solid fa-circle-plus" @click="goToContractAdd()"></i>
+
+        <!-- 狀態選擇清單 -->
+        <div class="statusList">
+        <select style="font-size: 22px;">
+            <option value="" disabled selected>房屋使用狀態篩選</option>
+            <option value="">出租中</option>
+            <option value="">空房</option>
+            <option value="">待生效</option>
+            <option value="">已結束</option>
+        </select>
+        </div>
+    </div>
+
+    <!--租約列表 con=contract -->
     <div class="conlist">
         <table>
-            <thead>
-                <tr>
-                    <th>選取</th>
-                    <th>編號</th>
-                    <th>承租人</th>
-                    <th>承租物件地址</th>
-                    <th>開始時間</th>
-                    <th>結束時間</th>
-                    <th>租金</th>
-                    <th>查看詳情</th>
-                </tr>
-            </thead>
-            <tbody>
-        <!-- 之後用v-for綁tr-->
-                <tr class="title">
-                    <!-- 這邊之後用item一個個去綁 -->
-                
-                    <td>{{  }}</td>
-                    <td>{{  }}</td>
-                    <td>{{  }}</td>
-                    <td>{{  }}</td>
-                    <td>{{  }}</td>
-                    <td>{{  }}</td>
-                    <td>{{  }}</td>
-                    
-                
-                    <td><button class="edit">查看</button></td>
-                </tr>
-            </tbody>
+        <thead>
+            <tr>
+            <th>選取</th>
+            <th>編號</th>
+            <th>承租人</th>
+            <th>狀態</th>
+            <th>承租物件地址</th>
+            <th>開始時間</th>
+            <th>結束時間</th>
+            <th>租金</th>
+            <th>查看詳情</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- 之後用v-for綁tr-->
+            <tr class="title">
+            <!-- 這邊之後用item一個個去綁 -->
+
+            <td>{{  }}</td>
+            <td>{{  }}</td>
+            <td>{{  }}</td>
+            <td>{{  }}</td>
+            <td>{{  }}</td>
+            <td>{{  }}</td>
+            <td>{{  }}</td>
+            <td>{{  }}</td>
+
+            <td><button class="edit">查看</button></td>
+            </tr>
+        </tbody>
         </table>
     </div>
-        
-</div>
-
-
+    </div>
 </template>
 
-<style scoped lang="scss">
-
-
-.manageArea{
-    width: 100dvw;
-    height: auto;
-    border: 1em solid  #9a685200;
-    //border-style:  inset;
-    margin-left: 10%;  // 移除 margin-left，並將 margin 設定為 auto
-    padding-top: 0;  // 確保 padding-top 為 0
-    background-color: #FAF0E9;
-}
-
-.searchPlace {
-    width: auto;
-    height: 20dvh;
-    color: black;
-    //border: 3px solid rgba(190, 188, 42, 0.822);
-    text-align: left;
-    margin: auto;
-    padding: 5% 0 0 20%;
-
-    .srColumn {
-        width: 30dvw;
+    <style scoped lang="scss">
+    .manageArea {
+        width: 100dvw;
+        height: auto;
+        border: 1em solid #9a685200;
+        //border-style:  inset;
+        margin-left: 10%;  // 移除 margin-left，並將 margin 設定為 auto
+        padding-top: 0;  // 確保 padding-top 為 0
+        background-color: #FAF0E9;
+    }
+//搜尋
+    .searchPlace {
+        width: auto;
+        height: 20dvh;
+        color: black;
+        font-size: 22px;
+        //border: 3px solid rgba(190, 188, 42, 0.822);
+        text-align: left;
+        margin: auto;
+        padding: 5% 0 0 20%;
     }
 
-    .qrsrtime {
+    .inputPlace {
+        display: flex; /* 使用 Flexbox 使元素並排 */
+        align-items: center; /* 垂直居中對齊 */
+        margin-bottom: 10px; /* 可以根據需要調整間距 */
+    }
+
+    .InputContainer {
+        width: 620px;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: linear-gradient(to bottom, #FF9D60, rgb(255, 231, 231));
+        border-radius: 30px;
+        overflow: hidden;
+        cursor: pointer;
+        box-shadow: 2px 2px 10px rgba(163, 162, 162, 0.075);
+        display: inline-block;
+        vertical-align: middle; /* Optional: Align vertically */
+        // flex: 1; /* 彈性增長，使 input 容器占據剩餘空間 */
+    }
+
+    .input {
+        width: 600px;
+        height: 40px;
+        caret-color: rgb(255, 81, 0);
+        background-color: rgb(255, 255, 255);
+        border-radius: 30px;
+        padding-left: 15px;
+        letter-spacing: 0.8px;
+        color: rgb(19, 19, 19);
+        font-size: 22px;
+    }
+
+    .datetime {
         margin-top: 20px;
     }
-
+//搜尋按鈕
     .searchbtn {
         margin-left: 3%;
         width: 90px;
         height: 40px;
         color: white;
+        font-size: 22px;
         border: 2px solid white;
         border-radius: 50px;
         background-color: #9A6852;
         transition: 0.2s;
-       
-        
-        }
-        .searchbtn:hover {
-            color:#9A6852;
-            background-color: #eed89a;
-            transform: scale(1.1); /* 在hover時縮放按鈕 */
-
-        }
-
-        .searchbtn:active {
-            color: #9A6852;
-            font-size: 600;
-            font-size: medium;
-            transform: scale(0.95); /* 在active時縮小按鈕 */
-            
-
     }
-}
 
+    .searchbtn:hover {
+        color: #9A6852;
+        background-color: #eed89a;
+        transform: scale(1.1); /* 在hover時縮放按鈕 */
+    }
 
-.conlist {
-    width: 100%;
-    height: auto;
-    // margin-left: 25%;
-    color: black;
-    background-color: antiquewhite;
-    border: none;
-    text-align: center;
-    overflow: hidden;
-    border-radius: 0px 0px 0px 0px;
-    box-shadow: 0 0 10px rgba(167, 147, 147, 0.541);
+    .searchbtn:active {
+        color: #9A6852;
+        font-size: 600;
+        font-size: medium;
+        transform: scale(0.95); /* 在active時縮小按鈕 */
+    }
 
+    .conlist {
+        width: 95%;
+        height: auto;
+        margin-top: 1%;
+        color: black;
+        background-color: antiquewhite;
+        border: none;
+        text-align: center;
+        overflow: hidden;
+        border-radius: 0px 0px 0px 0px;
+        box-shadow: 0 0 10px rgba(167, 147, 147, 0.541);
+    }
 
     table {
-    font-family: 'Oswald', sans-serif;
-    //collapse 會使儲存格邊框不出現。
-    border-collapse: collapse;
-
-}
+        font-family: 'Oswald', sans-serif;
+        border-collapse: collapse;
+    }
 
     th {
         background-color: #FFC89A;
@@ -186,22 +230,39 @@ export default {
         border-bottom: 2px solid rgb(173, 102, 60);
     }
 
-    //nth-of-type(even) 建立灰色背景，建立交錯的效果
     tr:nth-of-type(even) td {
         background-color: #f3f3f3;
-
     }
 
-}
+    .container {
+        display: flex;
+        margin-top: 1%;
+    }
 
+    .fa-trash-can {
+        font-size: 2.2em;
+        margin-right: 2%;
+        margin-top: 3%;
+    }
 
+    .fa-circle-plus {
+        font-size: 2.2em;
+        margin-right: 2%;
+        margin-top: 3%;
+    }
 
+    .statusList {
+        font-size: 22px;
+        margin-left: 72.5%;
+        margin-top: 3%;
+    }
 
-
-
-
+    .statusList option {
+        font-size: 22px;
+    }
+    
 </style>
-
+  
 
 
 
