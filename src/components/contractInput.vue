7cca20cb2d9ec2c4437546b5538c4bfe2a5f600f
Jiuan1231456 <coss2481@gmail.com>
2024-07-03 21:31:49 +0800

    契約輸入欄參考樣式就好了，方法不用理他;完成契約資料傳輸到資料庫功能

--- src/components/contractInput.vue
+++ src/components/contractInput.vue
@@ -1,29 +1,32 @@
 <script>
 export default {
     name: 'contractInput',
+    props: {
+        modelValue: String
+    },
+    methods: {
+        updateValue(event) {
+            this.$emit('update:modelValue', event.target.value);
+        }
+    }
 };
 </script>
 
 <template>
-
 <div class="input-wrapper">
-    <input class="input-box" type="textarea" placeholder="Enter your text">
+    <input class="input-box" type="text" :value="modelValue" @input="updateValue" placeholder="Enter your text">
     <span class="underline"></span>
 </div>
-
-
-
-
 </template>
 
-    <style scoped lang="scss">
-    .input-wrapper {
+<style scoped lang="scss">
+.input-wrapper {
     position: relative;
     width: 600px;
     margin: auto;
-    }
+}
 
-    .input-box {
+.input-box {
     font-size: 20px;
     padding: 10px 0;
     border: none;
@@ -32,9 +35,9 @@ export default {
     width: 100%;
     background-color: transparent;
     transition: border-color 0.3s ease-in-out;
-    }
+}
 
-    .underline {
+.underline {
     position: absolute;
     bottom: 0;
     left: 0;
@@ -43,16 +46,14 @@ export default {
     background-color: #FF9D60;
     transform: scaleX(0);
     transition: transform 0.3s ease-in-out;
-    }
+}
 
-    .input-box:focus {
+.input-box:focus {
     border-color: #FF9D60;
     outline: none;
-    }
+}
 
-    .input-box:focus + .underline {
+.input-box:focus + .underline {
     transform: scaleX(1);
-    }
-
-
+}
 </style>
--- src/views/ContractAdd.vue
+++ src/views/ContractAdd.vue
@@ -5,13 +5,44 @@ import { mapState, mapActions } from "pinia";
 //import print from 'print-js';
 
 import contractInput from '../components/contractInput.vue';
-// import dateInput from '../components/dateInput.vue';
 import preview_btn from '../components/preview_btn.vue';
 import send_btn from '../components/send_btn.vue';
 
 export default {
     data(){
         return {
+            tenant_identity:"",  
+            tenant_name:"",
+            tenant_home_address:"",
+            tenant_contact_address:"",
+            tenant_phone:"",
+            tenant_email:"",
+            owner_name:"",//從註冊資訊抓
+            owner_identity:"", //從註冊資訊抓
+            owner_home_address:"",//從註冊資訊抓
+            owner_contact_address:"",//從註冊資訊抓
+            owner_phone:"",//從註冊資訊抓
+            start_date:"",
+            end_date:"",
+            c_other:"",
+            sign_date:"",
+            cut_reason:"",
+            cut_date:"",
+            //===================================================
+             //房間資訊抓
+            address:"",
+            floor: "",
+            r_id: "",
+            rent_p: "",
+            deposit: "",
+            cut_p: "",
+            eletric_p: "",
+            water_p: "",
+            manage_p: "",
+            acreage: "",
+            parking: false,
+            equip: "",
+            r_other:""
 
         }
     },
@@ -19,8 +50,62 @@ export default {
         ...mapState(dataStore, ['page'])
     },
     methods: {
-        ...mapActions(dataStore,['setPage'])
-    },
+        // test(paramFromSon){
+        //     this.tenantName = paramFromSon;
+        //     console.log(this.tenantName);
+        // },
+        ...mapActions(dataStore,['setPage']),
+        addContractToDB(){
+            // console.log(this.tenantName);
+            // return;
+            let testObj={
+            tenantIdentity:this.tenantIdentity,  
+            tenantName:this.tenantName,
+            tenantHomeAddress:this.tenantHomeAddress,
+            tenantContactAddress:this.tenantContactAddress,
+            tenantPhone:this.tenantPhone,
+            tenantEmail:this.tenantEmail,
+            ownerName:this.ownerName,//從註冊資訊抓
+            ownerIdentity:this.ownerIdentity, //從註冊資訊抓
+            ownerHomeAddress:this.ownerHomeAddress,//從註冊資訊抓
+            ownerContactAddress:this.ownerContactAddress,//從註冊資訊抓
+            ownerPhone:this.ownerPhone,//從註冊資訊抓
+            startDate:this.startDate,
+            endDate:this.endDate,
+            cOther:this.cOther,
+            signDate:this.signDate,
+            cutReason:this.cutReason,
+            cutDate:this.cutDate,
+            ///從房間抓
+            address: this.address,
+            floor: this.floor,
+            roomId: this.roomId, //我的在java中的req檔是roomId，小徐的是rId，這邊先用小徐的
+            rentP: this.rentP,
+            deposit: this.deposit,
+            cutP: this.cutP,
+            eletricP: this.eletricP,
+            waterP: this.waterP,
+            manageP: this.manageP,
+            acreage: this.acreage,
+            parking: this.parking,
+            equip: this.equip,
+            signDate:this.signDate,
+            rOther:this.r_other
+
+            }
+            fetch("http://localhost:8080/contract/createContract", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(testObj)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                })
+            }
+        },
     components:{
         contractInput,
         // dateInput,
@@ -43,30 +128,34 @@ export default {
     <br>
     <div class="rent_time">
         <label for="start_time">租賃期間 自：</label>
-        <input type="date" id="start" style="font-size: 22px;" min="1970-01-01"max="2050-12-31" />
+        <input type="date" id="start" style="font-size: 22px;" min="1970-01-01"max="2050-12-31" v-model="this.startDate"/>
         <label for="end_time">到：</label>
-        <input type="date" id="end" style="font-size: 22px;" min="1970-01-01" max="2050-12-31" />
+        <input type="date" id="end" style="font-size: 22px;" min="1970-01-01" max="2050-12-31" v-model="this.endDate" />
 
 
     </div>
 
     <br>
-    門牌:<contractInput/>
+    租賃物件地址:<input type="text" v-model="this.address" class="input-box">
+    
     <br>
-    樓層:<contractInput/>
+    樓層:<input type="text" v-model="this.floor" class="input-box">
+   
     <br>
-    房號:<contractInput/>
+    房號:<input type="text" v-model="this.roomId" class="input-box">
+
     <br>
 
     
-    <br>租金/月: <contractInput/>
-    <br>押金: <contractInput/>
-    <br>管理費/月:<contractInput/> 
-    <br>電費/度: <contractInput/>
-    <br>水費/月:<contractInput/> 
+    <br>租金/月: <input type="text" v-model="this.rentP" class="input-box">
+    <br>押金: <input type="text" v-model="this.deposit" class="input-box">
+    <br>管理費/月:<input type="text" v-model="this.manageP" class="input-box">
+    <br>電費/度: <input type="text" v-model="this.eletricP" class="input-box">
+    <br>水費/月:<input type="text" v-model="this.waterP" class="input-box">
+    <br>面積:<input type="text" v-model="this.acreage" class="input-box">
     <br>物件備註:
     <div class="input-wrapper">
-    <textarea class="input-box" type="textarea" placeholder="Enter your text"></textarea>
+    <textarea class="input-box" type="textarea" placeholder="Enter your text" v-model="this.r_other"></textarea>
     <span class="underline"></span>
 </div>
     
@@ -76,29 +165,44 @@ export default {
 
     <h2><br>立契約書人</h2>
     <h3><br>
-        出租人姓名: <contractInput/>
+        出租人姓名: <input type="text" v-model="this.ownerName" class="input-box" >
+       
     </h3>
     <div class="Info">
-        身分證字號:<contractInput/>
+        身分證字號:<input type="text" v-model="this.ownerIdentitye" class="input-box">
+        
         <br>
-        戶籍地址(營業登記地址):<contractInput/>
+        戶籍地址(營業登記地址):<input type="text" v-model="this.ownerHomeAddress" class="input-box">
+      
         <br>
-        通訊地址:<contractInput/>
+        通訊地址:<input type="text" v-model="this.ownerContactAddress" class="input-box">
+       
         <br>
-        連絡電話:<contractInput/>
+        連絡電話:<input type="text" v-model="this.tenantName" class="input-box">
+        
     
     <h3>
         <br>
-        承租人姓名: <contractInput/>
+        承租人姓名: 
+        <input type="text" v-model="this.tenantName" class="input-box">
+        <!-- <contractInput @sendRequest="test"/> -->
+        <!-- 承租人姓名: <contractInput v-model="this.tenantName"/> -->
     </h3>
 
-        身分證字號:<contractInput/>
+        身分證字號:<input type="text" v-model="this.tenantIdentity" class="input-box">
+     
         <br>
-        戶籍地址(營業登記地址):<contractInput/>
+        戶籍地址(營業登記地址):<input type="text" v-model="this.tenantHomeAddress" class="input-box">
+ 
         <br>
-        通訊地址:<contractInput/>
+        通訊地址:<input type="text" v-model="this.tenantContactAddress " class="input-box">
+       
         <br>
-        連絡電話:<contractInput/>
+        email:<input type="text" v-model="this.tenantEmail" class="input-box">
+     
+        <br>
+        連絡電話:<input type="text" v-model="this.tenantPhone" class="input-box">
+        
 
     <!-- <h3><br>保證人： <contractInput/></h3>
         姓名： <contractInput/>
@@ -115,25 +219,26 @@ export default {
     </div>
     <h3><br>契約中止: </h3>
     <div class="cut"><br>
-        中止原因:<contractInput/>
+        中止原因:<input type="text" v-model="this.cutReason" class="input-box">
         <br>
-        違約金:<contractInput/>
+        違約金:<input type="text" v-model="this.cutP" class="input-box">
         <br>
-        中止日期:<contractInput/>
+        中止日期:<input type="text" v-model="this.cutDate" class="input-box">
     </div>
 </p>
     <h3><br>其他備註(或個別磋商條款):</h3>
     <div class="input-wrapper">
-    <textarea class="input-box" type="textarea" placeholder="Enter your text"></textarea>
+    <textarea class="input-box" type="textarea" placeholder="Enter your text" v-model="cOther" ></textarea>
     <span class="underline"></span>
 </div>
-    <h3><br>立約日期:<contractInput/></h3>
+    <h3><br>立約日期:<input type="date" id="start" style="font-size: 22px;" min="1970-01-01"max="2050-12-31" v-model="this.signDate"/>
+        </h3>
 
     <div class="btn"> 
         <preview_btn class="space-between"/>
-        <send_btn class="space-between"/>
 
-</div>
+        <send_btn class="space-between" @click="addContractToDB"/>
+    </div>
 </div>
 </template>
 
@@ -150,29 +255,23 @@ export default {
     .btn{
         margin-left: 22%;
         margin-bottom: 5%;
+        display: flex;
         .space-between { 
         margin-right: 30%;
         margin-top: 5%;}
     }
 }
 
-.input-wrapper {
-    position: relative;
+.input-box {
     width: 600px;
-    margin: auto;
-    }
-
-    .input-box {
     font-size: 20px;
     padding: 10px 0;
     border: none;
     border-bottom: 2px solid #ccc;
-    color: #201e1c;
+    color: #0f0f0f;
     width: 100%;
     background-color: transparent;
     transition: border-color 0.3s ease-in-out;
-    }
-
     .underline {
     position: absolute;
     bottom: 0;
@@ -182,16 +281,17 @@ export default {
     background-color: #FF9D60;
     transform: scaleX(0);
     transition: transform 0.3s ease-in-out;
-    }
+}
 
-    .input-box:focus {
+.input-box:focus {
     border-color: #FF9D60;
     outline: none;
-    }
+}
 
-    .input-box:focus + .underline {
+.input-box:focus + .underline {
     transform: scaleX(1);
-    }
+}
+}
 
 
 
