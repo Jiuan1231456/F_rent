4dc1687db81322f5c2e3d9b471e461b7582f7435
Jiuan1231456 <coss2481@gmail.com>
2024-07-04 14:50:14 +0800

    新增針對身份證字號抓取特定列表資訊的功能，但仍不可進行模糊搜尋

--- src/views/ContractAdd.vue
+++ src/views/ContractAdd.vue
@@ -1,36 +1,33 @@
-<!-- src="print.js" -->
-<script>
+<!-- src="print.js" --><script>
 import dataStore from "@/stores/dataStore";
 import { mapState, mapActions } from "pinia";
-//import print from 'print-js';
 
 import contractInput from '../components/contractInput.vue';
 import preview_btn from '../components/preview_btn.vue';
 import send_btn from '../components/send_btn.vue';
 
 export default {
-    data(){
+    data() {
         return {
-            tenant_identity:"",  
-            tenant_name:"",
-            tenant_home_address:"",
-            tenant_contact_address:"",
-            tenant_phone:"",
-            tenant_email:"",
-            owner_name:"",//從註冊資訊抓
-            owner_identity:"", //從註冊資訊抓
-            owner_home_address:"",//從註冊資訊抓
-            owner_contact_address:"",//從註冊資訊抓
-            owner_phone:"",//從註冊資訊抓
-            start_date:"",
-            end_date:"",
-            c_other:"",
-            sign_date:"",
-            cut_reason:"",
-            cut_date:"",
-            //===================================================
-             //房間資訊抓
-            address:"",
+            tenant_identity: "",  
+            tenant_name: "",
+            tenant_home_address: "",
+            tenant_contact_address: "",
+            tenant_phone: "",
+            tenant_email: "",
+            owner_name: "",  //從註冊資訊抓
+            owner_identity: "", //從註冊資訊抓
+            owner_home_address: "", //從註冊資訊抓
+            owner_contact_address: "", //從註冊資訊抓
+            owner_phone: "",  //從註冊資訊抓
+            start_date: "",
+            end_date: "",
+            c_other: "",
+            sign_date: "",
+            cut_reason: "",
+            cut_date: "",
+        // 房間資訊抓
+            address: "",
             floor: "",
             r_id: "",
             rent_p: "",
@@ -42,206 +39,241 @@ export default {
             acreage: "",
             parking: false,
             equip: "",
-            r_other:""
-
-        }
-    },
+            r_other: ""
+    }
+},
     computed: {
         ...mapState(dataStore, ['page'])
     },
+    created() {
+        this.getRegisterData();
+        this.getRoomData();
+         // 從路由參數中獲取房間資訊
+        this.roomInfo = this.$route.params.roomInfo;
+    },
+    mounted() {
+        this.setPage(7);
+    },
     methods: {
-        // test(paramFromSon){
-        //     this.tenantName = paramFromSon;
-        //     console.log(this.tenantName);
-        // },
-        ...mapActions(dataStore,['setPage']),
-        addContractToDB(){
-            // console.log(this.tenantName);
-            // return;
-            let testObj={
-            tenantIdentity:this.tenantIdentity,  
-            tenantName:this.tenantName,
-            tenantHomeAddress:this.tenantHomeAddress,
-            tenantContactAddress:this.tenantContactAddress,
-            tenantPhone:this.tenantPhone,
-            tenantEmail:this.tenantEmail,
-            ownerName:this.ownerName,//從註冊資訊抓
-            ownerIdentity:this.ownerIdentity, //從註冊資訊抓
-            ownerHomeAddress:this.ownerHomeAddress,//從註冊資訊抓
-            ownerContactAddress:this.ownerContactAddress,//從註冊資訊抓
-            ownerPhone:this.ownerPhone,//從註冊資訊抓
-            startDate:this.startDate,
-            endDate:this.endDate,
-            cOther:this.cOther,
-            signDate:this.signDate,
-            cutReason:this.cutReason,
-            cutDate:this.cutDate,
-            ///從房間抓
+        ...mapActions(dataStore, ['setPage']),
+        async getRegisterData() {
+        try {
+            const response = await fetch('http://localhost:8080/rent/account');
+            const data = await response.json();
+            // 假設 API 返回的資料是這樣的結構: { owner_name: '...', owner_phone: '...', owner_email: '...' }
+            this.owner_name = data.owner_name;
+            this.owner_phone = data.owner_phone;
+            this.owner_email = data.owner_email;
+        } catch (error) {
+            console.error('Error fetching register data:', error);
+        }
+        },
+        async getRoomData() {
+        try {
+            const response = await fetch('http://localhost:8080/room/creatRoom1');
+            const data = await response.json();
+            // 假設 API 返回的資料是這樣的結構: { r_id: '...', address: '...', rent_p: '...' }
+            this.r_id = data.r_id;
+            this.address = data.address;
+            this.rent_p = data.rent_p;
+        } catch (error) {
+            console.error('Error fetching room data:', error);
+        }
+        },
+        addContractToDB() {
+        let testObj = {
+            tenantIdentity: this.tenant_identity,  
+            tenantName: this.tenant_name,
+            tenantHomeAddress: this.tenant_home_address,
+            tenantContactAddress: this.tenant_contact_address,
+            tenantPhone: this.tenant_phone,
+            tenantEmail: this.tenant_email,
+            ownerName: this.owner_name, //從註冊資訊抓
+            ownerIdentity: this.owner_identity, //從註冊資訊抓
+            ownerHomeAddress: this.owner_home_address, //從註冊資訊抓
+            ownerContactAddress: this.owner_contact_address, //從註冊資訊抓
+            ownerPhone: this.owner_phone, //從註冊資訊抓
+            startDate: this.start_date,
+            endDate: this.end_date,
+            cOther: this.c_other,
+            signDate: this.sign_date,
+            cutReason: this.cut_reason,
+            cutDate: this.cut_date,
+            //從房間抓
             address: this.address,
             floor: this.floor,
-            roomId: this.roomId, //我的在java中的req檔是roomId，小徐的是rId，這邊先用小徐的
-            rentP: this.rentP,
+            roomId: this.r_id,
+            rentP: this.rent_p,
             deposit: this.deposit,
-            cutP: this.cutP,
-            eletricP: this.eletricP,
-            waterP: this.waterP,
-            manageP: this.manageP,
+            cutP: this.cut_p,
+            eletricP: this.eletric_p,
+            waterP: this.water_p,
+            manageP: this.manage_p,
             acreage: this.acreage,
             parking: this.parking,
             equip: this.equip,
-            signDate:this.signDate,
-            rOther:this.r_other
-
-            }
-            fetch("http://localhost:8080/contract/createContract", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(testObj)
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                })
-            }
-        },
-    components:{
+            signDate: this.sign_date,
+            rOther: this.r_other
+        };
+        fetch("http://localhost:8080/contract/createContract", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify(testObj)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        });
+        }
+    },
+    components: {
         contractInput,
-        // dateInput,
         preview_btn,
         send_btn,
     },
-    mounted(){
-        this.setPage(7)
-    }
-}
-</script >
+};
+</script>
 
 <template>
-<div class="bigArea">
-    <h1>新增契約書</h1>
-<p>
-    <div class="roomInfo">
-        <br>
-    <h2>租賃物件資訊</h2>
-    <br>
-    <div class="rent_time">
-        <label for="start_time">租賃期間 自：</label>
-        <input type="date" id="start" style="font-size: 22px;" min="1970-01-01"max="2050-12-31" v-model="this.startDate"/>
-        <label for="end_time">到：</label>
-        <input type="date" id="end" style="font-size: 22px;" min="1970-01-01" max="2050-12-31" v-model="this.endDate" />
-
-
-    </div>
-
-    <br>
-    租賃物件地址:<input type="text" v-model="this.address" class="input-box">
+    <div class="bigArea">
+        <h1>新增契約書</h1>
+        <div class="roomInfo">
+            <h2>租賃物件資訊</h2>
+            <div class="rent_time">
+            <label for="start_time">租賃期間 自：</label>
+            <input type="date" id="start" style="font-size: 22px;" min="1970-01-01" max="2050-12-31" v-model="start_date"/>
+            <label for="end_time">到：</label>
+            <input type="date" id="end" style="font-size: 22px;" min="1970-01-01" max="2050-12-31" v-model="end_date" />
+            </div>
+            <br>
+            租賃物件地址: <input type="text" v-model="address" class="input-box">
+            <br>
+            樓層: <input type="text" v-model="floor" class="input-box">
+            <br>
+            房號: <input type="text" v-model="r_id" class="input-box">
+            <br>
+            租金/月: <input type="text" v-model="rent_p" class="input-box">
+            <br>
+            押金: <input type="text" v-model="deposit" class="input-box">
+            <br>
+            管理費/月: <input type="text" v-model="manage_p" class="input-box">
+            <br>
+            電費/度: <input type="text" v-model="eletric_p" class="input-box">
+            <br>
+            水費/月: <input type="text" v-model="water_p" class="input-box">
+            <br>
+            面積: <input type="text" v-model="acreage" class="input-box">
+            <br>
+            物件備註:
+            <div class="input-wrapper">
+            <textarea class="input-box" type="textarea" placeholder="Enter your text" v-model="r_other"></textarea>
+            <span class="underline"></span>
+            </div>
+        </div>
     
-    <br>
-    樓層:<input type="text" v-model="this.floor" class="input-box">
-   
-    <br>
-    房號:<input type="text" v-model="this.roomId" class="input-box">
-
-    <br>
-
+        <h2>立契約書人</h2>
+        <div class="Info">
+            出租人姓名: <input type="text" v-model="owner_name" class="input-box">
+            <br>
+            身分證字號: <input type="text" v-model="owner_identity" class="input-box">
+            <br>
+            戶籍地址(營業登記地址): <input type="text" v-model="owner_home_address" class="input-box">
+            <br>
+            通訊地址: <input type="text" v-model="owner_contact_address" class="input-box">
+            <br>
+            連絡電話: <input type="text" v-model="owner_phone" class="input-box">
+            <br>
+            承租人姓名: <input type="text" v-model="tenant_name" class="input-box">
+            <br>
+            身分證字號: <input type="text" v-model="tenant_identity" class="input-box">
+            <br>
+            戶籍地址(營業登記地址): <input type="text" v-model="tenant_home_address" class="input-box">
+            <br>
+            通訊地址: <input type="text" v-model="tenant_contact_address" class="input-box">
+            <br>
+            email: <input type="text" v-model="tenant_email" class="input-box">
+            <br>
+            連絡電話: <input type="text" v-model="tenant_phone" class="input-box">
+        </div>
     
-    <br>租金/月: <input type="text" v-model="this.rentP" class="input-box">
-    <br>押金: <input type="text" v-model="this.deposit" class="input-box">
-    <br>管理費/月:<input type="text" v-model="this.manageP" class="input-box">
-    <br>電費/度: <input type="text" v-model="this.eletricP" class="input-box">
-    <br>水費/月:<input type="text" v-model="this.waterP" class="input-box">
-    <br>面積:<input type="text" v-model="this.acreage" class="input-box">
-    <br>物件備註:
-    <div class="input-wrapper">
-    <textarea class="input-box" type="textarea" placeholder="Enter your text" v-model="this.r_other"></textarea>
-    <span class="underline"></span>
-</div>
+        <h3>契約中止</h3>
+        <div class="cut">
+            <br>
+            中止原因: <input type="text" v-model="cut_reason" class="input-box">
+            <br>
+            違約金: <input type="text" v-model="cut_p" class="input-box">
+            <br>
+            中止日期: <input type="text" v-model="cut_date" class="input-box">
+        </div>
     
-
-    </div>
-
-
-    <h2><br>立契約書人</h2>
-    <h3><br>
-        出租人姓名: <input type="text" v-model="this.ownerName" class="input-box" >
-       
-    </h3>
-    <div class="Info">
-        身分證字號:<input type="text" v-model="this.ownerIdentitye" class="input-box">
-        
-        <br>
-        戶籍地址(營業登記地址):<input type="text" v-model="this.ownerHomeAddress" class="input-box">
-      
-        <br>
-        通訊地址:<input type="text" v-model="this.ownerContactAddress" class="input-box">
-       
-        <br>
-        連絡電話:<input type="text" v-model="this.tenantName" class="input-box">
-        
+        <h3>其他備註(或個別磋商條款)</h3>
+        <div class="input-wrapper">
+            <textarea class="input-box" type="textarea" placeholder="Enter your text" v-model="c_other"></textarea>
+            <span class="underline"></span>
+        </div>
     
-    <h3>
-        <br>
-        承租人姓名: 
-        <input type="text" v-model="this.tenantName" class="input-box">
-        <!-- <contractInput @sendRequest="test"/> -->
-        <!-- 承租人姓名: <contractInput v-model="this.tenantName"/> -->
-    </h3>
-
-        身分證字號:<input type="text" v-model="this.tenantIdentity" class="input-box">
-     
-        <br>
-        戶籍地址(營業登記地址):<input type="text" v-model="this.tenantHomeAddress" class="input-box">
- 
-        <br>
-        通訊地址:<input type="text" v-model="this.tenantContactAddress " class="input-box">
-       
-        <br>
-        email:<input type="text" v-model="this.tenantEmail" class="input-box">
-     
-        <br>
-        連絡電話:<input type="text" v-model="this.tenantPhone" class="input-box">
-        
-
-    <!-- <h3><br>保證人： <contractInput/></h3>
-        姓名： <contractInput/>
-        <br>
-        統一編號(身分證明文件編號)：<contractInput/>
-        <br>
-        戶籍地址：<contractInput/>
-        <br>
-        通訊地址：<contractInput/>
-        <br>
-        聯絡電話：<contractInput/> -->
-        
-
+        <h3>立約日期:</h3>
+        <input type="date" id="sign_date" style="font-size: 22px;" min="1970-01-01" max="2050-12-31" v-model="sign_date"/>
+    
+        <div class="btn"> 
+            <preview_btn class="space-between"/>
+            <send_btn class="space-between" @click="addContractToDB"/>
+        </div>
     </div>
-    <h3><br>契約中止: </h3>
-    <div class="cut"><br>
-        中止原因:<input type="text" v-model="this.cutReason" class="input-box">
-        <br>
-        違約金:<input type="text" v-model="this.cutP" class="input-box">
-        <br>
-        中止日期:<input type="text" v-model="this.cutDate" class="input-box">
-    </div>
-</p>
-    <h3><br>其他備註(或個別磋商條款):</h3>
-    <div class="input-wrapper">
-    <textarea class="input-box" type="textarea" placeholder="Enter your text" v-model="cOther" ></textarea>
-    <span class="underline"></span>
-</div>
-    <h3><br>立約日期:<input type="date" id="start" style="font-size: 22px;" min="1970-01-01"max="2050-12-31" v-model="this.signDate"/>
-        </h3>
-
-    <div class="btn"> 
-        <preview_btn class="space-between"/>
-
-        <send_btn class="space-between" @click="addContractToDB"/>
-    </div>
-</div>
-</template>
-
+  </template>
+  
+  <style scoped lang="scss">
+  .bigArea {
+    width: 40%;
+    margin-top: 3%;
+    margin-left: 150px;
+    font-size: 18px;
+  
+    .btn {
+      margin-left: 22%;
+      margin-bottom: 5%;
+      display: flex;
+      .space-between { 
+        margin-right: 30%;
+        margin-top: 5%;
+      }
+    }
+  }
+  
+  .input-box {
+    width: 600px;
+    font-size: 20px;
+    padding: 10px 0;
+    border: none;
+    border-bottom: 2px solid #ccc;
+    color: #0f0f0f;
+    background-color: transparent;
+    transition: border-color 0.3s ease-in-out;
+  
+    .underline {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      background-color: #FF9D60;
+      transform: scaleX(0);
+      transition: transform 0.3s ease-in-out;
+    }
+  
+    &:focus {
+      border-color: #FF9D60;
+      outline: none;
+    }
+  
+    &:focus + .underline {
+      transform: scaleX(1);
+    }
+  }
+  </style>
+  
 <!-- PDF列印按鈕 -->
 <!-- <button type="button" onclick="printJS('docs/printjs.pdf')">列印PDF</button> -->
 <style scoped lang="scss">
--- src/views/ContractList.vue
+++ src/views/ContractList.vue
@@ -4,20 +4,87 @@ import { mapState, mapActions } from "pinia";
 export default {
     data(){
         return {
-
+            //儲存契約列表
+            contractList: [],
+            // 儲存搜尋對象過濾器
+            contractFilters: {
+                address: "",
+                tenantName:"",
+                startDate: "",
+                endDate: ""
+            },
+            selectedContracts: [] // 新增選中的契約狀態,用於存儲選中的契約。
+        
         }
     },
     computed: {
-        ...mapState(dataStore, ['page'])
+        // 綁定 Pinia 狀態
+        ...mapState(dataStore, ['page','loginObj'])
+        
+    
     },
     methods: {
-        ...mapActions(dataStore,['returnPage','setPage']),
+         // 綁定 Pinia actions
+        ...mapActions(dataStore,['setPage']),
+           // 跳轉到新增契約頁面
         goToContractAdd() {
             // 使用 Vue Router 的方式進行跳轉
-            this.$router.push({ name: 'contractAdd'}); // 假設路由設定中有名為 Back1 的路由
+            this.$router.push({ name: 'contractAdd'}); 
         },
+          //模糊搜尋過濾器
+        search() {
+               // 建立搜尋條件，依照地址、承租方姓名或租約日期進行搜尋
+            let searchObj = {
+                address: this.contractFilters.address,
+                tenantName:this.contractFilters.tenantName,
+                startDate: this.contractFilters.startDate,
+                endDate: this.contractFilters.endDate,
+            };
+            console.log("Search Object:", searchObj);// 打印搜尋條件以供調試
+
+            // 發送搜尋請求到後端
+            fetch("http://localhost:8080/contract/contratSearch", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(searchObj)
+            })
+                .then(res => res.json())//將回應轉換為 JSON
+                .then(data => {
+                    console.log("Response Data:", data);// 打印回應資料以供調試
+
+                    // 篩選出當前身份證字號的契約問卷
+                    this.contractList = data.contractList.filter(item => item.ownerIdentity === this.loginObj.ownerIdentity);
+                    console.log("Filtered Contract List:", this.contractList);
+        
+                    // 計算總頁數
+                    this.calculateTotalPages(this.contractList.length)
+                })
+                .catch(error => {
+                console.error("Error fetching data:", error); // 處理錯誤
+            });
+        },
+          // 計算總頁數
+        calculateTotalPages(totalItems) {
+            const pageSize = 10; // 假設每頁顯示 10 筆資料
+            const totalPages = Math.ceil(totalItems / pageSize);
+            console.log("Total Pages:", totalPages); // 打印總頁數以供調試
+        },
+        //新增查看契約詳細資訊的方法
+        viewContractDetails(contract) {
+            // 這裡可以導航到契約詳細資訊頁面或彈出模態窗口來顯示詳細資訊
+            console.log("Viewing contract details:", contract);
+            // 例如：
+            // this.$router.push({ name: 'contractDetails', params: { id: contract.id } });
+        }
+
     },
+    created(){
+        this.search(); // 組件創建時執行搜尋以獲取初始數據
+    },   
     mounted(){
+        // 設定初始頁面
         this.setPage(6)
     }
 }
@@ -45,13 +112,12 @@ export default {
         <input type="date" id="start" name="trip-start" min="1970-01-01" max="2050-12-31" style="font-size: 22px;">
         <label for="end_time" style="background-color:  #FFC89A;">到：</label>
         <input type="date" id="end" name="trip-end" min="1970-01-01" max="2050-12-31" style="font-size: 22px;">
-        <button class="searchbtn" type="button">搜尋</button>
+        <button class="searchbtn" type="button" @click="search()">搜尋</button>
         </p>
     </div>
 
     <div class="container">
-        <!-- 新增/刪除租約 -->
-        <i class="fa-solid fa-trash-can"></i>
+        <!-- 新增租約按鈕 -->
         <i class="fa-solid fa-circle-plus" @click="goToContractAdd()"></i>
 
         <!-- 狀態選擇清單 -->
@@ -83,21 +149,21 @@ export default {
             </tr>
         </thead>
         <tbody>
-            <!-- 之後用v-for綁tr-->
-            <tr class="title">
-            <!-- 這邊之後用item一個個去綁 -->
-
-            <td>{{  }}</td>
-            <td>{{  }}</td>
-            <td>{{  }}</td>
-            <td>{{  }}</td>
-            <td>{{  }}</td>
-            <td>{{  }}</td>
-            <td>{{  }}</td>
-            <td>{{  }}</td>
-
-            <td><button class="edit">查看</button></td>
+            <!-- item 是在 v-for 循環中定義的一個臨時變量，用來表示 contractList 陣列中的每個元素 -->
+            <!-- index 是每次迭代過程中的當前索引值。在這裡是指 contractList 陣列中每個元素的索引位置。 -->
+            <tr v-for="(item, index) in this.contractList" :key="index">
+                <td><input type="checkbox" v-model="selectedContracts" :value="item.roomId"></td>
+                <td>{{ item.roomId }}</td>
+                <td>{{ item.tenantName }}</td>
+                <td>{{ item.status }}</td>
+                <td>{{ item.address }}</td>
+                <!-- 這邊還要再寫另一個方法來獲取狀態 -->
+                <td>{{ item.startDate }}</td>
+                <td>{{ item.endDate }}</td>
+                <td>{{ item.rentP }}</td>
+                <td><button @click="viewContractDetails(contract)">查看</button></td>
             </tr>
+            
         </tbody>
         </table>
     </div>
@@ -247,21 +313,15 @@ export default {
         margin-top: 1%;
     }
 
-    .fa-trash-can {
-        font-size: 2.2em;
-        margin-right: 2%;
-        margin-top: 3%;
-    }
-
     .fa-circle-plus {
         font-size: 2.2em;
-        margin-right: 2%;
         margin-top: 3%;
+        margin-left: -5%;
     }
 
     .statusList {
         font-size: 22px;
-        margin-left: 72.5%;
+        margin-left: 85%;
         margin-top: 3%;
     }
 
